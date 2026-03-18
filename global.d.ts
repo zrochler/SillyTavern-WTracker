bf448cfd56a982d62552fda21bd29d1f57ebc3cf
Zach <zrochler@gmail.com>
2026-03-17 17:42:50 -0700

    Strip outgoing reasoning

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+export {};
+
+// 1. Import for user-scoped extensions
+import '../../../../public/global';
+// 2. Import for server-scoped extensions
+import '../../../../global';
+
+// Define additional types if needed...
+declare global {
+    // Add global type declarations here
+}
